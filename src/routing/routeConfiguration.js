72988ae78183e3fbc8404ccf472a1379deb05cbf
Aman <gs420126@gmail.com>
2026-03-19 14:49:23 +0530

    test landing

--- src/routing/routeConfiguration.js
+++ src/routing/routeConfiguration.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import loadable from '@loadable/component';
-
+import LandingPage from './pages/LandingPage';
 import getPageDataLoadingAPI from '../containers/pageDataLoadingAPI';
 import NotFoundPage from '../containers/NotFoundPage/NotFoundPage';
 import PreviewResolverPage from '../containers/PreviewResolverPage/PreviewResolverPage';
